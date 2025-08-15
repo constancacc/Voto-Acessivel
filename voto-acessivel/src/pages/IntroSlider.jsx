701ac2d42fdbd185fcc84c597d2384decb82ede5
constancacc <constancamfcorreia@gmail.com>
2025-08-15 11:13:16 +0100

    css botões de selecionar eleição

--- voto-acessivel/src/pages/IntroSlider.jsx
+++ voto-acessivel/src/pages/IntroSlider.jsx
@@ -33,7 +33,7 @@ export default function IntroSlider() {
               <path d="M103.889 26.7837L76.6375 60.3311L61.3887 45.6726" stroke="black" strokeWidth="8.87205" strokeLinecap="round" strokeLinejoin="round"/>
               <path d="M4.72266 24.7559H70.833C72.8085 24.7559 74.41 26.3576 74.4102 28.333C74.4102 30.3086 72.8086 31.9102 70.833 31.9102H62.5693C54.7649 31.9102 48.4385 38.2376 48.4385 46.042V80.2773C48.4385 82.2528 46.8368 83.8544 44.8613 83.8545C42.8858 83.8545 41.2842 82.2529 41.2842 80.2773V59.0273C41.2839 57.0911 39.7136 55.5215 37.7773 55.5215C35.8412 55.5217 34.2717 57.0912 34.2715 59.0273V80.2773C34.2715 82.2529 32.6699 83.8545 30.6943 83.8545C28.7188 83.8544 27.1172 82.2529 27.1172 80.2773V46.042C27.1172 38.2376 20.7907 31.9103 12.9863 31.9102H4.72266C2.74709 31.9102 1.14551 30.3086 1.14551 28.333C1.14568 26.3576 2.7472 24.7559 4.72266 24.7559ZM37.7773 1.14551C42.3609 1.14551 46.0771 4.86081 46.0771 9.44434C46.0771 14.0279 42.3609 17.7441 37.7773 17.7441C33.194 17.7439 29.4785 14.0278 29.4785 9.44434C29.4786 4.86095 33.194 1.14574 37.7773 1.14551Z" fill="black" stroke="black" strokeWidth="2.29028"/>
             </svg>
-            <p aria-hidden="true" style={{ color: "#000", fontSize: "40px", fontWeight: 700, marginTop: "1rem" }}>
+            <p aria-hidden="true" style={{ color: "#000", fontSize: "40px", fontWeight: 700, marginTop: "1rem", width: "100%", textAlign: "center", lineHeight: "1.2" }}>
               Voto Acessível
             </p>
           </div>
@@ -51,16 +51,16 @@ export default function IntroSlider() {
     {
       content: (
         <div className="slide-content" data-slide-index={1}>
-          <div style={{ gridColumn: "3 / span 9", gridRow: "3" }}>
+          <div style={{ gridColumn: "3 / span 14", gridRow: "3" }}>
             <h1 tabIndex={0}>Informações de Leitor de Ecrã</h1>
           </div>
-          <div style={{ gridColumn: "5 / span 6", gridRow: "5", padding: "1rem" }}>
+          <div style={{ gridColumn: "5 / span 14", gridRow: "5", padding: "1rem" }}>
             <img tabIndex={0} src="/imagens/instrucoes/leitor-de-ecra.svg" alt="Leitor de Ecrã"/>
           </div>
-          <div style={{ gridColumn: "3 / span 7", gridRow: "8", alignSelf: "end" }}>
+          <div style={{ gridColumn: "3 / span 14", gridRow: "8", alignSelf: "end" }}>
             <h3 tabIndex={0}>Navegação com leitor de ecrã</h3>
           </div>
-          <div style={{ gridColumn: "3 / span 10", gridRow: "9" }}>
+          <div style={{ gridColumn: "3 / span 14", gridRow: "9" }}>
             <p tabIndex={0}>
               A plataforma possui um <b>leitor de ecrã</b> que é ativado <b>automaticamente</b> ao iniciar,
               <b> lendo todos os elementos</b> apresentados.
@@ -78,13 +78,13 @@ export default function IntroSlider() {
           <div style={{ gridColumn: "4 / span 6", gridRow: "5", padding: "1rem" }}>
             <img tabIndex={0} src="/imagens/instrucoes/definicoes de acessibilidade.svg" alt="Logo Voto Acessível" className="logo-svg"/>
           </div>
-          <div style={{ gridColumn: "3 / span 7", gridRow: "8", alignSelf: "end" }}>
+          <div style={{ gridColumn: "3 / span 14", gridRow: "8", alignSelf: "end" }}>
             <h3 tabIndex={0}>Personalize a sua experiência</h3>
           </div>
           <div style={{ gridColumn: "3 / span 10", gridRow: "9" }}>
             <p tabIndex={0}>
               Pode ajustar o <b>tamanho</b> e o <b>tipo de letra</b>, as <b>cores</b> e o <b>idioma</b> de leitura.
-              Todas estas opções foram pensadas para melhorar a compreensão e navegação na plataforma.
+              
             </p>
           </div>
         </div>
@@ -220,7 +220,7 @@ export default function IntroSlider() {
       )}
 
       {/* Contador de tempo restante */}
-      <div style={{ gridColumn: "5 / span 5", gridRow: "11", marginTop: "1rem"}}>
+      <div style={{ gridColumn: "5 / span 5", gridRow: "11"}}>
         <p className="slider-page-text" tabIndex={0} style={{ marginLeft: "1rem" }}>
           {activeIndex < slides.length - 1
             ? `Faltam ${timeLeft}s para a próxima página`
